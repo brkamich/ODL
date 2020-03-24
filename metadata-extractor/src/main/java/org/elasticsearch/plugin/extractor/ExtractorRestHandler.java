2a90c6dd7090bd2ac74f0180fd0d4e522b7377ca
Michal Brka <michal.brka@notix.cz>
2020-03-24 10:06:21 +0100

    added PDFModule for extraction and base schema for plugin

--- metadata-extractor/src/main/java/org/elasticsearch/plugin/extractor/ExtractorRestHandler.java
+++ metadata-extractor/src/main/java/org/elasticsearch/plugin/extractor/ExtractorRestHandler.java
@@ -1,23 +1,23 @@
 package org.elasticsearch.plugin.extractor;
 
 import org.elasticsearch.client.node.NodeClient;
-import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.*;
+import org.elasticsearch.plugin.extractor.objects.InfoHolder;
 import org.elasticsearch.rest.*;
 import org.elasticsearch.common.inject.Inject;
 
 import java.io.IOException;
-
-import static org.elasticsearch.rest.RestRequest.Method.GET;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ExtractorRestHandler extends BaseRestHandler {
     private final String NAME = "_extract_metadata";
     @Inject
     public ExtractorRestHandler(Settings settings, RestController restController) {
         super();
-        restController.registerHandler(RestRequest.Method.GET, "/"+NAME, this);
+        restController.registerHandler(RestRequest.Method.POST, "/"+NAME, this);
+        restController.registerHandler(RestRequest.Method.PUT, "/"+NAME, this);
     }
 
     @Override
@@ -27,10 +27,15 @@ public class ExtractorRestHandler extends BaseRestHandler {
 
     @Override
     protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
+        InfoHolder val_info = ExtractionController.extract(request,client);
+        XContentBuilder builder = XContentBuilder.builder(XContentType.JSON.xContent()).startObject().field("message",val_info.getValidation_message()).endObject();
+        Logger.getGlobal().log(Level.INFO,val_info.toString());
         return channel -> {
-            XContentBuilder builder = channel.newBuilder();
-            builder.startObject().field("message", "Hello from extractor!").endObject();
-            channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder));
+                if(val_info.getResponse()!=null){
+                    channel.sendResponse(new BytesRestResponse(val_info.getResponse().status(), val_info.getResponse().toXContent(builder,ToXContent.EMPTY_PARAMS)));
+                }else{
+                    channel.sendResponse(new BytesRestResponse(val_info.getStatus(),builder));
+                }
         };
     }
 }
